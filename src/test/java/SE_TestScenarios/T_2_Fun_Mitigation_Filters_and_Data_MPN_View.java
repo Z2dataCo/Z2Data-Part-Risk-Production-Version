76cf7609417c646923bfcf5ab96fdca0649e0d26
zayatoo <ahmedzayat2311@gmail.com>
2021-01-13 09:48:24 +0200

    AZ1312021|9:48AM Updating T_2 Assertions

--- src/test/java/SE_TestScenarios/T_2_Fun_Mitigation_Filters_and_Data_MPN_View.java
+++ src/test/java/SE_TestScenarios/T_2_Fun_Mitigation_Filters_and_Data_MPN_View.java
@@ -34,6 +34,8 @@ public class T_2_Fun_Mitigation_Filters_and_Data_MPN_View   {
 
         Data_Management_Obj.Z2D_Move_to_Mitigation_BOM();
         Risk_Management_Obj.Z2D_Wait_Invisibility_Of_Z2Data_Spinner_1();
+        String NRND_Parts = driver.findElement(Risk_Management_Obj.NRND_Parts).getText().replace(" Parts","");
+        String End_of_Life_Parts = driver.findElement(Risk_Management_Obj.End_Of_Life_Parts).getText().replace(" Parts","");
         Risk_Management_Obj.Z2D_Mitigation_Scroll();
         while (driver.findElements(Risk_Management_Obj.Added_Part_Label_List).size() != 0) {
             Risk_Management_Obj.Z2D_Mitigation_Click_on_Close_Button();
@@ -69,25 +71,25 @@ public class T_2_Fun_Mitigation_Filters_and_Data_MPN_View   {
         //    Assert.assertEquals(TotalFilterValue + " Parts",Active_Parts );
 //has been Commented because of Bug ***********************
 
-        String NRND_Parts = driver.findElement(Risk_Management_Obj.NRND_Parts).getText();
+       // String NRND_Parts = driver.findElement(Risk_Management_Obj.NRND_Parts).getText();
         System.out.println("NRND_Filter_Value : " + NRND_Parts);
         Risk_Management_Obj.Z2D_Mitigation_NRND_Parts_to_be_Clickable();
         Risk_Management_Obj.Z2D_Mitigation_Click_on_NRND_Filter();
         Risk_Management_Obj.Z2D_Wait_Invisibility_Of_Z2Data_Spinner_1();
-        Risk_Management_Obj.Wait_Text_To_be(driver.findElement(Risk_Management_Obj.Forecast_Ele_Total_Value), "226");
+        Risk_Management_Obj.Wait_Text_To_be(driver.findElement(Risk_Management_Obj.Forecast_Ele_Total_Value), NRND_Parts);
         String TotalFilterValue2 = driver.findElement(Risk_Management_Obj.Forecast_Ele_Total_Value).getText();
         System.out.println("Total Filter Value : " + TotalFilterValue2);
-        Assert.assertEquals(TotalFilterValue2 + " Parts", NRND_Parts);
+        Assert.assertEquals(TotalFilterValue2 , NRND_Parts);
 
-        String End_of_Life_Parts = driver.findElement(Risk_Management_Obj.End_Of_Life_Parts).getText();
+      //  String End_of_Life_Parts = driver.findElement(Risk_Management_Obj.End_Of_Life_Parts).getText();
         System.out.println("End_Of_Life_Parts : " + End_of_Life_Parts);
         Risk_Management_Obj.Z2D_Mitigation_End_Of_Life_Parts_to_be_Clickable();
         Risk_Management_Obj.Z2D_Mitigation_Click_on_End_of_Life_Filter();
         Risk_Management_Obj.Z2D_Wait_Disappear_Z2Data_Spinner_0();
-        Risk_Management_Obj.Wait_Text_To_be(driver.findElement(Risk_Management_Obj.Forecast_Ele_Total_Value), "248");
+        Risk_Management_Obj.Wait_Text_To_be(driver.findElement(Risk_Management_Obj.Forecast_Ele_Total_Value), End_of_Life_Parts);
         String TotalFilterValue3 = driver.findElement(Risk_Management_Obj.Forecast_Ele_Total_Value).getText();
         System.out.println("Total Filter Value : " + TotalFilterValue3);
-        Assert.assertEquals(TotalFilterValue3 + " Parts", End_of_Life_Parts);
+        Assert.assertEquals(TotalFilterValue3 , End_of_Life_Parts);
     }
     @AfterClass(alwaysRun = true)
     public void TearDown() {
